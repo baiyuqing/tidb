e8033643a732cee6fe4e7374640f8cfdde58efcb
baiyuqing <baiyuqing@zhihu.com>
2019-10-27 13:26:19 +0800

    update stree script

--- cmd/testsql/main.go
+++ cmd/testsql/main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/cactus/go-statsd-client/statsd"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -17,6 +19,8 @@ var (
 	addr string
 	proc int
 	sqls string
+
+	cuser string
 
 	max     float64
 	num     int
@@ -28,7 +32,9 @@ var (
 	throughputC chan int
 	quitC       chan struct{}
 
-	logger *log.Logger
+	logger  *log.Logger
+	pattern = "user.%v.metric.%v"
+	client  statsd.Statter
 )
 
 func init() {
@@ -40,6 +46,16 @@ func init() {
 	latencyC = make(chan float64, 10240)
 	throughputC = make(chan int, 10240)
 	quitC = make(chan struct{})
+	cuser = strings.Split(addr, ":")[0]
+
+	var err error
+	//client, err = statsd.NewClient("localhost:8126", "hack")
+	client, err = statsd.NewClient("status:8126", "hack")
+
+	if err != nil {
+		logger.Println(err)
+		return
+	}
 
 }
 
@@ -50,15 +66,15 @@ func printStatistics() {
 	fmt.Println("limited by throttle:", limited)
 }
 
-func execute(wg *sync.WaitGroup) {
+func execute(wg *sync.WaitGroup, db *sql.DB) {
 	defer wg.Done()
-	db, err := sql.Open("mysql", addr)
-	defer db.Close()
-	if err != nil {
-		panic(err)
-	}
+	// db, err := sql.Open("mysql", addr)
+	// defer db.Close()
+	// if err != nil {
+	// 	panic(err)
+	// }
 	start := time.Now()
-	_, err = db.Query(sqls)
+	rows, err := db.Query(sqls)
 	if err != nil {
 		// fmt.Println("query err:", err)
 		logger.Println("query err:", err)
@@ -66,6 +82,9 @@ func execute(wg *sync.WaitGroup) {
 		l.Lock()
 		limited = limited + 1
 		l.Unlock()
+	}
+	if rows != nil {
+		defer rows.Close()
 	}
 	elapsed := time.Since(start)
 	elapsedSec := elapsed.Seconds()
@@ -90,8 +109,8 @@ func handleChannel() {
 	go func() {
 		<-signalChan
 
-		close(quitC)
 		printStatistics()
+		close(quitC)
 		os.Exit(0)
 	}()
 }
@@ -104,7 +123,39 @@ func addLatency(d float64) {
 	latencyC <- d
 }
 
+func getQuota() {
+	db, err := sql.Open("mysql", addr)
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		rows, err := db.Query("select user_name,quota_limit from mysql.quota where user_name = 'hackathon';")
+		if err != nil {
+			// fmt.Println("query err:", err)
+			fmt.Println("query err:", err)
+			return
+		}
+		var user string
+		var quota_count int64
+		// data.Scan(dest ...interface{})
+		for rows.Next() {
+			if err := rows.Scan(&user, &quota_count); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		rows.Close()
+		// fmt.Printf("user: %v, quota: %v\n", user, quota_count)
+		client.Gauge(fmt.Sprintf(pattern, user, "quota-count"), quota_count, 1)
+		time.Sleep(10 * time.Second)
+	}
+
+}
+
 func showStatitistic() {
+
 	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 	var total, last int
@@ -128,6 +179,9 @@ func showStatitistic() {
 			lastElapsed = totalElapsed
 			fmt.Printf("%0.2f\tQPS, total: %v\n", qps, total)
 			fmt.Printf("%0.2f\tAvgLatency per sec, total: %v\n", avgLatency, totalElapsed)
+			client.Raw(fmt.Sprintf(pattern, cuser, "qps"), fmt.Sprintf("%0.2f|g", qps), 1)
+			client.Raw(fmt.Sprintf(pattern, cuser, "avg-latency"), fmt.Sprintf("%0.2f|g", avgLatency), 1)
+			// client.Gauge(fmt."u.%v", int64, f)
 		}
 	}
 }
@@ -137,7 +191,7 @@ func main() {
 	fmt.Println("mysql addr:", addr)
 	fmt.Println("proc number:", proc)
 	fmt.Println("sql to execute:", sqls)
-
+	go getQuota()
 	logfile, err := os.Create("./mysql-stress.log")
 	if err != nil && err != os.ErrExist {
 		fmt.Println(err)
@@ -147,11 +201,21 @@ func main() {
 
 	handleChannel()
 	go showStatitistic()
+	conns := make([]*sql.DB, proc+1)
+	for i := 0; i < proc; i++ {
+		db, err := sql.Open("mysql", addr)
+		defer db.Close()
+		if err != nil {
+			panic(err)
+		}
+		conns[i] = db
+	}
+
 	var wg sync.WaitGroup
 	for {
 		for i := 0; i < proc; i++ {
 			wg.Add(1)
-			go execute(&wg)
+			go execute(&wg, conns[i])
 		}
 		wg.Wait()
 		// time.Sleep(1 * time.Second)
